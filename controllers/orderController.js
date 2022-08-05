beba2b0efa1232e35a7f0d76939a527029681a05
Jessica Wang <jessicaw.wyt@gmail.com>
2022-08-05 11:09:44 -0400

    Added createOrder

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -6,14 +6,21 @@ const getOrders = async (req, res) => {
 };
 
 const createOrder = async (req, res) => {
-  if (req.body.totalPrice && req.body.isDelivery && req.body.isPickup) {
+  if (
+    req.body.totalPrice &&
+    (req.body.isDelivery || req.body.isPickup) &&
+    req.body.orders.length > 0
+  ) {
+    // create new order
     const order = await Order.create({
       totalPrice: req.body.totalPrice,
       isPickup: req.body.isPickup,
       isDelivery: req.body.isDelivery,
       user: req.user.id,
+      orders: req.body.orders,
     });
-    res.status(200).json(order);
+    if (order) res.status(200).json(order);
+    else res.status(500).json({ error: 'Info not valid' });
   } else {
     res.status(500).json({ error: 'Please provide valid info' });
   }
--- models/order.model.js
+++ models/order.model.js
@@ -5,9 +5,23 @@ const OrderSchema = new mongoose.Schema(
     totalPrice: { type: Number, required: true },
     isPickup: { type: Boolean, required: true },
     isDelivery: { type: Boolean, required: true },
+    orders: [
+      {
+        dishId: { type: mongoose.Schema.Types.ObjectId, ref: 'Dish' },
+        qty: { type: Number },
+        dishPrice: { type: Number },
+        sideId: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: 'Dish',
+          default: null,
+        },
+        sidePrice: { type: Number, default: 0 },
+      },
+    ],
     user: {
       type: mongoose.Schema.Types.ObjectId,
       ref: 'User',
+      required: true,
     },
   },
   {
